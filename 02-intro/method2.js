b18caab018a1ecc3625bd492101bfec2119d9ae8
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-09-27 11:25:35 +0530

    array methods

--- 02-intro/method2.js
+++ 02-intro/method2.js
@@ -12,3 +12,9 @@ console.log(num.sort((a, b) => a - b));
 
 console.log(num.sort((a, b) => b-a));
 
+
+console.log(
+    "check number every number  is greater than 1 or not ?",
+    num.every((values) => values > 1)
+);
+
