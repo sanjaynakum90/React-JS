2f0b1a6ef703d36b780d4a7653e8ef1b41d6a105
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-10-01 11:29:01 +0530

    intro of directry

---
+++ 04-react-directry/src/App.jsx
@@ -0,0 +1,10 @@
+const App = ()=>{
+  return(
+    <div>
+      <h1>sanjay nakum</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, ullam.</p>
+    </div>
+  )
+}
+
+export default App;
