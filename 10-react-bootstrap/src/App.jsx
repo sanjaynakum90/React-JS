117ca53962dabb629582c9f5d5f442933cb46d03
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-11-18 15:16:07 +0530

    template create

--- 10-react-bootstrap/src/App.jsx
+++ 10-react-bootstrap/src/App.jsx
@@ -1,52 +1,22 @@
 import React from 'react'
-
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Nav from 'react-bootstrap/Nav';
-
+import Navbar from "./Component/Navbar"
+import Carousel from "./Component/Carousel"
+import About from './Component/About'
 
 const App = () => {
   return (
+
     <>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Header>
-          <Nav variant="tabs" defaultActiveKey="#first">
-            <Nav.Item>
-              <Nav.Link href="#first">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#disabled" disabled>
-                Disabled
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <Carousel />
+        <About />
+      </main>
     </>
-
-
-  );
+  )
 }
 
 export default App
