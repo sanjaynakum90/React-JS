760ed9f92e759a047c8885c5ca3bdccca39cb2dc
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-01-08 15:33:36 +0530

    login with google authentication

--- 16-react-FireBase/Config/config.js
+++ 16-react-FireBase/Config/config.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth , GoogleAuthProvider} from "firebase/auth"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_STROGE_BUCKET,
-    messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app)
+
+export const googleProvider = new GoogleAuthProvider();
--- 16-react-FireBase/src/Components/Auth.jsx
+++ 16-react-FireBase/src/Components/Auth.jsx
@@ -1,21 +1,34 @@
 import { useState } from "react";
-
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
-const Auth = () => {
-    const [isLogin, setIsLogin] = useState("");
+import { auth } from "../../Config/config";
 
-    const [data, setData] = useState({
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup,
+} from "firebase/auth";
+
+const Auth = () => {
+    const [isLogin, setIsLogin] = useState(true);
+
+    const [user, setUser] = useState("");
+
+    const [authData, setAuthData] = useState({
         email: "",
         password: "",
     });
 
-    const handleInput = (identifier, e) => {
-        setData((prev) => {
+    const [loading, setLoading] = useState(false);
+
+    const handleChange = (identifier, e) => {
+        setAuthData((prev) => {
             return {
                 ...prev,
                 [identifier]: e.target.value,
@@ -23,48 +36,82 @@ const Auth = () => {
         });
     };
 
-    const handleSubmit = async (e) => {
+    const handleForm = async (e) => {
         e.preventDefault();
+
+        setLoading(true);
         try {
-
+            if (isLogin) {
+                const result = await signInWithEmailAndPassword(
+                    auth,
+                    authData.email,
+                    authData.password
+                );
+                setUser(result.user.email);
+            } else {
+                const result = await createUserWithEmailAndPassword(
+                    auth,
+                    authData.email,
+                    authData.password
+                );
+                setUser(result.user.email);
+            }
         } catch (error) {
+            console.log(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
+    const handleGoogleLogin = async () => {
+        setLoading(true);
+        try {
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            setUser(result.user.email);
+        } catch (error) {
+            console.log(error.message);
+            alert(error.message);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
         <>
-            <Container className="mt-5">
-                <Row className="d-flex justify-content-center">
-                    <Col md={6}>
-                        <Form className="border p-3" onSubmit={handleSubmit}>
-                            <h2 className="text-center">{isLogin ? "Login" : "Signin"}</h2>
-                            <Form.Group
-                                className="mb-3"
-                                controlId="exampleForm.ControlInput1"
-                            >
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder="name@example.com"
-                                    onChange={(e) => handleInput("email", e)}
-                                />
-                            </Form.Group>
-                            <Form.Group
-                                className="mb-4"
-                                controlId="exampleForm.ControlInput1"
-                            >
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Password"
-                                    onChange={(e) => handleInput("password", e)}
-                                />
-                            </Form.Group>
-                            <div className="d-flex flex-column justify-content-center align-items-center">
-                                <Button className="w-50 mb-3" type="submit"> {isLogin ? "Login" : "Signin"} </Button>
-                                <Button className="w-50">{isLogin ? "Welcome" : "Create Account"}</Button>
-                            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Form
+                            className="d-flex justify-content-center align-items-center"
+                            style={{ height: "100vh" }}
+                            onSubmit={handleForm}
+                        >
+                            <Card className="shadow p-3 " style={{ width: "30%" }}>
+                                <h4 className="text-center">{isLogin ? "Login" : "Sign up"}</h4>
+                                {user && <p className="alert alert-primary">{user}</p>}
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control value={authData.email} onChange={(e) => handleChange("email", e)} placeholder="Email" >
+                                    </Form.Control>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control value={authData.password} placeholder="Password" onChange={(e) => handleChange("password", e)}>
+                                    </Form.Control>
+                                </Form.Group>
+
+                                <Button className="btn btn-primary btn-success" type="submit">
+                                    {isLogin ? "login" : "sign up"}
+                                </Button>
+                                <button className="btn btn-danger mt-3" type="submit" onClick={handleGoogleLogin} disabled={loading}>
+                                    Login with google
+                                </button>
+                                <Button className="btn btn-primary mt-3" onClick={() => setIsLogin(!isLogin)}>
+                                    {isLogin ? "New User ? Sign Up" : "Already User ? Login"}
+                                </Button>
+                            </Card>
                         </Form>
                     </Col>
                 </Row>
