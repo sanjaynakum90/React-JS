482bc1d365332159eb6a464ce774da3f454dc1e3
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-01-21 11:02:27 +0530

    update

--- 17-react-Travel-booking/src/main.jsx
+++ 17-react-Travel-booking/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AuthContext } from './Context/AuthContext.jsx';
+import AuthProvider from './Context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthContext.Provider>
+    <AuthProvider>
       <App />
-    </AuthContext.Provider>
+    </AuthProvider>
   </StrictMode>,
 )
